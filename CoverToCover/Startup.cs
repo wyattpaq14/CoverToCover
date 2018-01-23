fa27773c5bb615844f8010be9f980d8c8b9d0dea
001253831 <001253831@N210E4>
2018-01-22 19:05:07 -0500

    Add project files.

---
+++ CoverToCover/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoverToCover.Startup))]
+namespace CoverToCover
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
